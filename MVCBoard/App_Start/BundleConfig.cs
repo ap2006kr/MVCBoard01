106f6c2d148e4da26d878b3e335d30c55a802484
Jack <ap2006kr@hotmail.com>
2019-09-20 00:02:48 +0900

    달력 달기 성공

--- MVCBoard/App_Start/BundleConfig.cs
+++ MVCBoard/App_Start/BundleConfig.cs
@@ -8,8 +8,8 @@ namespace MVCBoard
         // 묶음에 대한 자세한 내용은 https://go.microsoft.com/fwlink/?LinkId=301862를 참조하세요.
         public static void RegisterBundles(BundleCollection bundles)
         {
-          //  bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-          //              "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -17,7 +17,9 @@ namespace MVCBoard
             // Modernizr의 개발 버전을 사용하여 개발하고 배우십시오. 그런 다음
             // 프로덕션에 사용할 준비를 하고 https://modernizr.com의 빌드 도구를 사용하여 필요한 테스트만 선택하세요.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-*",
+                        "~/Scripts/moment.js"
+                        ));
 
           //  bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
             //          "~/Scripts/bootstrap.js"));
@@ -35,22 +37,21 @@ namespace MVCBoard
                 "~/Content/theme01/vendor/fontawesome-free/css/all.min.css",
                 "~/Content/theme01/vendor/datatables/dataTables.bootstrap4.css",
                 "~/Content/theme01/css/sb-admin.css", 
-                "~/Content/theme01/css/Site.css?13123"
+                "~/Content/theme01/css/Site.css?13123",
+                "~/Content/bootstrap-datetimepicker.css"
                 ));
 
             bundles.Add(new ScriptBundle("~/bundles/theme01").Include(
-                     "~/Content/theme01/vendor/jquery/jquery.min.js",
+                    // "~/Content/theme01/vendor/jquery/jquery.min.js",
                       "~/Content/theme01/vendor/bootstrap/js/bootstrap.bundle.min.js",
                        "~/Content/theme01/vendor/jquery-easing/jquery.easing.min.js",
                         
                          "~/Content/theme01/vendor/datatables/jquery.dataTables.js",
                           "~/Content/theme01/vendor/datatables/dataTables.bootstrap4.js",
                            "~/Content/theme01/js/sb-admin.min.js",
-                            "~/Content/theme01/js/demo/datatables-demo.js"
+                            "~/Content/theme01/js/demo/datatables-demo.js",
                         //    "~/Content/theme01/js/demo/chart-area-demo.js"
-
-
-
+                               "~/Scripts/bootstrap-datetimepicker.js"
                      ));
 
 
